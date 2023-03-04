b5298d55bdb24bf22c952670396d448ca403694f
Tazeem Hassan <126955250+tazeem786@users.noreply.github.com>
2023-03-04 16:16:24 +0500

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -8,12 +8,41 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
 Route::get('/', function () {
-    return view('welcome');
-    
+    return view('posts');
+});
+Route::get('/posts', function () {
+    return view('posts');
+});
+Route::get('/createpost', function () {
+    return view('createpost');
+});
+Route::get('/profile', function () {
+    return view('profile');
+});
+Route::get('/wallet', function () {
+    return view('wallet');
+});
+Route::get('/productdetail', function () {
+    return view('productdetail');
+});
+Route::get('/publicprofile', function () {
+    return view('publicprofile');
+});
+Route::get('/login', function () {
+    return view('login');
+});
+Route::get('/register', function () {
+    return view('register');
+});
+Route::get('/codeverify', function () {
+    return view('codeauth');
+});
+Route::get('/forgetpassword', function () {
+    return view('forgetpassword');
 });
